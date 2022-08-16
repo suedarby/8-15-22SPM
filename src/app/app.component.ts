555e76a90b8c7dd1178154f13826cbc43a0543ce
suedarby <sue.a.darby@gmail.com>
2022-08-16 11:28:12 -0800

    dash, login & project modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core'; //original
 
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+
 @Component({ //original
   selector: 'app-root', //original
   templateUrl: './app.component.html', //original
   styleUrls: ['./app.component.css'] //original
 }) //original
 export class AppComponent { //original
-  title = '8-15-22SPM'; //original
+  title = 'Sewing Project Manager'; //original
+  facoffee = faCoffee;
 } //original
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser'; //original
 
 import { AppRoutingModule } from './app-routing.module'; //original
 import { AppComponent } from './app.component'; //original
+import { faFontAwesome } from '@fortawesome/free-solid-svg-icons';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ProjectsComponent } from './projects/projects.component';
+import { DashComponent } from './dash/dash.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({ //original
-  declarations: [ //original
-    AppComponent //original
-  ], //original
-  imports: [ //original
-    BrowserModule, //original
-    AppRoutingModule //original
+  declarations: [		//original
+    AppComponent, //original
+    ProjectsComponent,
+    DashComponent,
+      LoginComponent
+   ], //original
+
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    // ProjectsComponent,
+    // DashComponent
   ], //original
   providers: [], //original
   bootstrap: [AppComponent] //original
